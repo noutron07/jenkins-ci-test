69f42aa85b784a24c7f6ae6ad92cd3eb959252c5
Velican Erdoğan <velicanerdogan0@hotmail.com>
2022-03-13 18:27:29 +0300

    application port change

--- MinimalAPI/Program.cs
+++ MinimalAPI/Program.cs
@@ -2,6 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.WebHost.UseUrls("http://*:54321");
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
